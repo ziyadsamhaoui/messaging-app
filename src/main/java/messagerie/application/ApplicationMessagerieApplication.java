cc7359124585abe6471a537ee7e3bebe3b52a921
ismailboulahya <boulahyaismail257@gmail.com>
2026-02-18 13:55:24 +0000

    first

---
+++ src/main/java/messagerie/application/ApplicationMessagerieApplication.java
@@ -0,0 +1,13 @@
+package messagerie.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationMessagerieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMessagerieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/messagerie/application/ApplicationMessagerieApplicationTests.java
@@ -0,0 +1,13 @@
+package messagerie.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationMessagerieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
